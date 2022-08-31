2aca843260e76e53594ec0d9037f39ed0ec0a66a
Simone Fortunato <frtsmn94@gmail.com>
2022-08-31 08:18:54 +0200

    Inizio Esercizio

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+}
+</script>
+
+<style lang="scss">
+    @import '../style/general.scss';
+    header{
+        height: 100px;
+        background-color:black
+    }
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:'MyMain'
+}
+</script>
+
+<style>
+
+</style>
